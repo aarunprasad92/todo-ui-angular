0703831db9138d485f6f032d578ef9f3d4f03e1c
Arun Prasad Anbalagan <aarunprasad92@gmail.com>
2024-12-20 11:26:35 +0530

    Make component standalone and use it in another component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, WelcomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-welcome',
   imports: [],
+  standalone: true,
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
