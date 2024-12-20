24706fa6685ce6f732d9f151bd5034e2c8002a79
Arun Prasad Anbalagan <aarunprasad92@gmail.com>
2024-12-20 12:50:35 +0530

    Add two way binding using ngModel and added click event handler

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  userName: string = '';
+  password: string = '';
 
+  // Event handler method
+  onLogin(): void {
+    console.log('Login button clicked');
+    console.log('Username:', this.userName);
+    console.log('Password:', this.password);
+  }
 }
