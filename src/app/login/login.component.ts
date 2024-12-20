14460d9384ef5f90c65839b68113660524aa26b4
Arun Prasad Anbalagan <aarunprasad92@gmail.com>
2024-12-20 12:59:57 +0530

    Use ngIf from the angular common module to conditionally render ui elements

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,20 +1,29 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   userName: string = '';
   password: string = '';
+  errorMessage: string = '';
 
   // Event handler method
   onLogin(): void {
-    console.log('Login button clicked');
-    console.log('Username:', this.userName);
-    console.log('Password:', this.password);
+    const validUsername = 'admin';
+    const validPassword = '123';
+
+    if (this.userName === validUsername && this.password === validPassword) {
+      this.errorMessage = '';
+      console.log('Login successful');
+      // Add your login logic here
+    } else {
+      this.errorMessage = 'Invalid credentials';
+    }
   }
 }
